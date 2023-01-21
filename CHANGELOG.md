24637c66f6b2049f671e4beba050b4f836aba2f3
hugodcrq <hugodecroocq@gmail.com>
2023-01-22 00:29:37 +0100

    feat: :sparkles: add skeleton service directive and styles - add github actions workflows
    * :sparkles: add skeleton service directive and styles - add github actions workflows ([fc8fa81](https://github.com/hugodcrq/ngx-skeleton/commit/fc8fa8183b5069df2e3c0c91484e428a9408b383))

---
+++ CHANGELOG.md
@@ -0,0 +1,6 @@
+# 1.0.0 (2023-01-22)
+
+
+### Features
+
+* :sparkles: add skeleton service directive and styles - add github actions workflows ([fc8fa81](https://github.com/hugodcrq/ngx-skeleton/commit/fc8fa8183b5069df2e3c0c91484e428a9408b383))
--- projects/hugodcrq/ngx-skeleton/src/public-api.ts
+++ projects/hugodcrq/ngx-skeleton/src/public-api.ts
@@ -1,7 +1,6 @@
 /*
  * Public API Surface of ngx-skeleton
  */
-
 export * from './lib/ngx-skeleton.service';
-export * from './lib/ngx-skeleton.component';
+export * from './lib/ngx-skeleton.directive';
 export * from './lib/ngx-skeleton.module';
