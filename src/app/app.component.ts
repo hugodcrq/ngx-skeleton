56316b5b0a98581c0b8970079a35abc868fa16f0
hugodcrq <hugodecroocq@gmail.com>
2023-01-24 10:51:18 +0100

    feat: :sparkles: improve demo app and update README

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, ElementRef, inject, OnInit, ViewChild } from '@angular/core';
 import { NgxSkeletonService } from '@hugodcrq/ngx-skeleton';
 
 @Component({
@@ -7,19 +7,41 @@ import { NgxSkeletonService } from '@hugodcrq/ngx-skeleton';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
+  @ViewChild('progressValue') progressValue: ElementRef<HTMLElement>;
+
   title = 'ngx-skeleton';
   skeleton = inject(NgxSkeletonService);
+  timerInterval;
+  timerStart = 0;
+  timerCurrentValue = 0;
+  timerCurrentPercent: string = '';
+  isReady = false;
 
   ngOnInit() {
+    this.startTimer();
     this.skeleton.show();
 
     // simulate long task (3s) and hide
     setTimeout(() => {
+      this.clearTimer();
       this.skeleton.hide();
-    }, 10000);
+      this.isReady = true;
+    }, 4000);
   }
 
   reload() {
     window.location.reload();
+  }
+
+  startTimer() {
+    this.timerInterval = setInterval(() => {
+      this.timerCurrentValue += 1;
+      this.progressValue.nativeElement.style.width = `${(this.timerCurrentValue * 100) / 3}%`;
+    }, 1000);
+  }
+
+  clearTimer() {
+    clearInterval(this.timerInterval);
+    this.timerCurrentValue = this.timerStart;
   }
 }
